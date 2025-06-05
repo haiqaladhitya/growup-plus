8ff85bef81daeebcf5fcc2788390563ee0dad61e
haiqaladhitya <101262063+haiqaladhitya@users.noreply.github.com>
2025-06-05 09:16:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -68,10 +68,10 @@ def plot_progress(actual, ideal, label, unit):
     fig.update_layout(
         height=300,  # Tinggi pie chart
         width=300,   # Lebar pie chart
-        margin=dict(t=20, b=20, l=20, r=20)  # Memberikan margin untuk memperkecil chart
+        margin=dict(t=20, b=20, l=15, r=15)  # Memberikan margin untuk memperkecil chart
     )
     
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, use_container_width=False)
 
 
 # mapping rekomendasi berdasarkan (Stunting, Wasting)
